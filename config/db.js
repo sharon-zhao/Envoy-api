367199e42fa98195fb6c2a68f6907f2fe87e415c
sharon zhao <xinrui.zhao@yahoo.com>
2020-06-07 13:29:08 -0400

    change name of previous

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'group-project-api'
+const mongooseBaseName = 'Envoy-api'
 
 // create the mongodb uri for development and test
 const database = {
